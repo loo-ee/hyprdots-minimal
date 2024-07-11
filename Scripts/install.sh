401a6640bbe9b1b6053c020685bc9f8f6fa0e082
Jann Louie Almirante <jannlouiealmirante@gmail.com>
2024-07-11 20:26:38 +0800

    Fix themepatcher list

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -155,7 +155,7 @@ EOF
         [ -d "${themePath}" ] || mkdir -p "${themePath}"
         [ -f "${themePath}/.sort" ] || echo "${#themeNameQ[@]}" > "${themePath}/.sort"
     done < "${scrDir}/themepatcher.lst"
-    parallel --bar --link "${scrDir}/themepatcher.sh" "{1}" "{2}" "{3}" "{4}" ::: "${themeNameQ[@]}" ::: "${themeRepoQ[@]}" ::: "--skipcaching" ::: "false"
+    parallel --bar --link "${scrDir}/themepatcher.sh" "{1}" ::: "${themeNameQ[@]}" ::: "${themeRepoQ[@]}" ::: "--skipcaching" ::: "false"
     echo -e "\n\033[0;32m[cache]\033[0m generating cache files..."
     "$HOME/.local/share/bin/swwwallcache.sh" -t ""
     if printenv HYPRLAND_INSTANCE_SIGNATURE &> /dev/null; then
